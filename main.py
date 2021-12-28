657419852dd1fd23a3de220c2386868246014d9f
vtitoff <v.titoff2010@gmail.com>
2021-12-28 21:49:51 +0300

    add telebot

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import requests as r
 import json
+import telebot
 
 token = ''
-url = f'https://api.telegram.org/bot{token}/getMe'
+bot = telebot.TeleBot(token)
 
-# https://api.telegram.org/bot<token>/method
-print(r.get(url).json())
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+bot.infinity_polling()
