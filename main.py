f40ba999feb87b8b5e8a913f5882a5cb7650c7b7
vtitoff <v.titoff2010@gmail.com>
2021-12-28 20:30:48 +0300

    fix requirements and url

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import requests as r
 import json
 
-url = 'https://api.telegram.org'
 token = ''
+url = f'https://api.telegram.org/bot{token}/getMe'
+
 # https://api.telegram.org/bot<token>/method
-print(r.get(url, token=token).json())
+print(r.get(url).json())
