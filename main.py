e214e9493b01a62e742c9e11d90d3e75798c6d28
vtitoff <v.titoff2010@gmail.com>
2022-01-06 23:15:08 +0300

    working sum coins

--- main.py
+++ main.py
@@ -4,33 +4,32 @@ import telebot
 import os
 import sqlite3
 
-# db = sqlite3.connect("bot_database.db")
-
-
 token = os.environ['motivate_bot_token']
 bot = telebot.TeleBot(token)
 
 
-# sql = "INSERT INTO users VALUES ('603970011', 0)"
-# cursor.execute(sql)
-
 def get_coins(id):
     db = sqlite3.connect("bot_database.db")
-    # sql = f"SELECT coins FROM users WHERE user_id={id}"
-    sql = "SELECT coins FROM users"
+    sql = f"SELECT coins FROM users WHERE user_id={id}"
     cursor = db.cursor()
     cursor.execute(sql)
-    print(cursor.fetchall())
-    return cursor.fetchall() or 'anus'
+    for x in cursor.fetchone():
+        return x
+
 
 def add_coins(id):
     db = sqlite3.connect("bot_database.db")
     cursor = db.cursor()
-    sql = "INSERT INTO users(user_id, coins) VALUES ('603970011', 1)"
+    sql = f"SELECT coins FROM users WHERE user_id={id}"
+    cursor = db.cursor()
     cursor.execute(sql)
-    db.commit()
-    print(cursor.fetchall())
-    return cursor.fetchall() or 'hmmm'
+    for x in cursor.fetchone():
+        sql = f"UPDATE users SET coins=coins+1 WHERE user_id = {id};"
+        cursor.execute(sql)
+        db.commit()
+        break
+    return 'Добавлена монетка'
+
 
 @bot.message_handler(commands=['start'])
 def button(message):
@@ -51,7 +50,6 @@ def callback(call):
             bot.send_message(call.message.chat.id, get_coins(call.message.chat.id))
         elif call.data == 'button_add_score':
             bot.send_message(call.message.chat.id, add_coins(call.message.chat.id))
-
 
 
 bot.infinity_polling()
