5a3b559ca8b61d03888a4d6812a30203c1f3190c
vtitoff <v.titoff2010@gmail.com>
2022-01-07 15:37:22 +0300

    add score buttons and menu button

--- main.py
+++ main.py
@@ -39,16 +39,31 @@ def button(message):
     button_add_award = telebot.types.InlineKeyboardButton('Добавить награду', callback_data='button_add_award')
     button_info = telebot.types.InlineKeyboardButton('Инфо', callback_data='button_info')
     markup.add(button_add_score, button_list_awards, button_add_award, button_info)
-    bot.send_message(message.chat.id, f"Главное меню \nВ наличии {get_coins(message.chat.id)} баллов", reply_markup=markup)
+    bot.send_message(message.chat.id, f"Главное меню \nВ наличии {get_coins(message.chat.id)} баллов",
+                     reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback(call):
     if call.message:
         if call.data == 'button_add_score':
+            print(call.message)
             bot.send_message(call.message.chat.id, add_coins(call.message.chat.id))
+            markup = telebot.types.InlineKeyboardMarkup(row_width=2)
+            button_1 = telebot.types.InlineKeyboardButton('1', callback_data='button_add_1')
+            button_2 = telebot.types.InlineKeyboardButton('2', callback_data='button_add_2')
+            button_3 = telebot.types.InlineKeyboardButton('3', callback_data='button_add_3')
+            button_4 = telebot.types.InlineKeyboardButton('4', callback_data='button_add_4')
+            button_5 = telebot.types.InlineKeyboardButton('5', callback_data='button_add_5')
+            menu = telebot.types.InlineKeyboardButton('Назад в меню', callback_data='back_to_menu')
+            markup.add(button_1, button_2, button_3, button_4, button_5, menu)
+            bot.send_message(call.message.chat.id, f"Главное меню \nВ наличии {get_coins(call.message.chat.id)} баллов",
+                             reply_markup=markup)
         elif call.data == 'button_list_awards':
-            pass
-
+            bot.edit_message_text(text='тест', chat_id=call.message.chat.id, message_id=call.message.message_id)
+        elif call.data == 'button_add_1':
+            bot.send_message(call.message.chat.id, f"Тест кнопки")
+        elif call.data == 'back_to_menu':
+            button(call.message)
 
 bot.infinity_polling()
